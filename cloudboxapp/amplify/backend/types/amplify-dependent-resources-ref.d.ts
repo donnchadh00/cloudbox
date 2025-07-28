eac5d81d573731901e985e5420f0196777611c20
donnchadh00 <donnchadhgriffin00@gmail.com>
2025-07-28 23:58:53 +0100

    amplify vite react template

---
+++ cloudboxapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cloudboxappc5845a23": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
