db64ea5f9884cde74be5fa733542c08410f04749
donnchadh00 <donnchadhgriffin00@gmail.com>
2025-07-29 15:12:18 +0100

    add login and signup functionality

---
+++ cloudboxapp/src/FileManager.jsx
@@ -0,0 +1,7 @@
+function FileManager() {
+  return (
+      <h1>File Manager</h1>
+  )
+}
+
+export default FileManager
--- cloudboxapp/src/main.jsx
+++ cloudboxapp/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify'
 import App from './App.jsx'
+import './index.css'
+import amplifyConfig from './amplifyconfiguration.json'
+import '@aws-amplify/ui-react/styles.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+Amplify.configure(amplifyConfig)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
 )
